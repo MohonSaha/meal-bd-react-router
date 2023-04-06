692bfa4a56b4b3772fe5e130a51794558696cada
MohonSaha <practicewithmohon@gmail.com>
2023-04-06 18:43:20 +0600

    add button hover effect

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h2>I am about</h2>
+        <div className='about-container'>
+            <div>
+                <p>1. Biryani - The King of Rice Dishes</p>
+                <p>Hailing from the Indian subcontinent, Biryani is a fragrant and flavorful rice dish that is fit for royalty. Made with aromatic spices, succulent meat (such as chicken, lamb, or beef), and fragrant basmati rice, Biryani is a symphony of flavors that are layered together to create a truly exquisite experience. The slow-cooking process ensures that each grain of rice is infused with the rich flavors of the spices and meat, making every bite a culinary delight.</p>
+             </div>
+
+             <div>
+                <p>2. Sushi - The Art of Japanese Cuisine </p>
+                <p>Sushi, a quintessential dish from Japan, is a true work of art that is as visually stunning as it is delicious. Made with fresh, raw fish or seafood, perfectly seasoned rice, and a touch of wasabi, sushi is a delicacy that requires precision and skill to master. Each piece of sushi is carefully crafted by skilled chefs, who take pride in creating a harmonious balance of flavors and textures. From the creamy richness of toro (fatty tuna) to the briny sweetness of uni (sea urchin), sushi is a sensory experience that delights both the palate and the eyes.</p>
+             </div>
+
+             <div>
+                <p>3.Coq au Vin - The French Classic</p>
+                <p>Coq au Vin is a classic French dish that epitomizes rustic elegance. This slow-cooked chicken stew is made with red wine, bacon, mushrooms, onions, and aromatic herbs, resulting in a rich and savory flavor profile that is both hearty and comforting. The chicken is braised to perfection, allowing the flavors to meld together and create a sumptuous sauce that is ideal for soaking up with a crusty baguette. Coq au Vin is a timeless dish that represents the art of French cooking and is sure to transport you to the charming streets of Paris with every bite.</p>
+             </div>
+
+             <div>
+                <p>4. Tiramisu - The Italian Delight</p>
+                <p>No culinary journey would be complete without a taste of Italy, and Tiramisu is the perfect indulgence for dessert lovers. This iconic Italian dessert is made with layers of espresso-soaked ladyfingers, rich mascarpone cheese, and a sprinkle of cocoa powder. The result is a luscious and creamy dessert that is both sweet and tangy, with the perfect balance of flavors. Tiramisu is a true showstopper that is sure to impress and satisfy your sweet tooth.</p>
+             </div>
+
+
+
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css'
 import Lottie from "lottie-react";
 import food from '../../assets/food.json'
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPizzaSlice } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
     return (
@@ -11,13 +13,16 @@ const Home = () => {
                 <p className='header'>Welcome to the Culinary Delights: <span className='special-header'>A Gastronomic Adventure!</span></p>
                 <p>Indulge in the world of flavors, aromas, and culinary creations with our food website, Culinary Delights! From classic recipes to modern twists, we are your ultimate destination for all things food. Our team of passionate food enthusiasts and experienced chefs work tirelessly to bring you the most mouthwatering recipes, culinary tips, and food-related news. With Culinary Delights, get ready to embark on a gastronomic adventure like no other. Whether you're a seasoned chef or a cooking novice, our website caters to all skill levels and taste preferences. We believe that food is not just about sustenance, but also a form of art that ignites our senses and brings people together.</p>
                 <div className='home-btn'>
-                    <NavLink className="food-button btn" to="/food">Visit Food</NavLink>
-                    <NavLink className="about-button btn" to="/about">About</NavLink>
-                    
+                    <NavLink className="food-button btn" to="/food">Visit Food <FontAwesomeIcon className='pizza-icon' icon={faPizzaSlice} />
+                    </NavLink>
+                    <div className='main-div'>
+                        <NavLink className="about-btn" to="/about">About</NavLink>
+                    </div>
+
                 </div>
             </div>
             <div className='anni-container'>
-            <Lottie animationData={food} />;
+                <Lottie animationData={food} />;
 
             </div>
         </div>
